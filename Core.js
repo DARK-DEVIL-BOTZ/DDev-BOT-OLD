fc97c2028771c93f4a0167c2f14f4541d87d03e0
TEAM DARK DEVIL <111672563+DARK-DEVIL-BOTZ@users.noreply.github.com>
2023-09-02 18:32:58 +0530

    💝DARK-DEVIL-BOTZ🧜‍♂️

--- Core.js
+++ Core.js
@@ -485,63 +485,45 @@ Typed *surrender* to surrender and admited defeat`
 const pickRandom = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)]
   }
-
-	
+  
 let smallinput = budy.toLowerCase()
+
 if (smallinput.includes('hello')) {
   reply (`Hello ${pushname}, I am ${BotName}. How can i help you?`);
 } 
 
-//if (smallinput.includes('hi')) {
-
-//   reply (`Hello ${pushname}, I am ${BotName}. How can i help you?`);
-
-// } 
-
-if (smallinput=='kai') {
+if (smallinput=='darkdevil') {
     reply (`My Boss is lost in another Multiverse, and I lost the connection with him...`)
 }
-
 
 if (smallinput=='runtime') {
   reply (`Hey ${pushname} my runtime is ${runtime(process.uptime())}\n\nCurrent Time: ${kaitime}\n\nCurrent Date: ${kaidate}`)
 }
 
-
-
 if( smallinput.includes('konichiwa') || smallinput.includes('konochiwa') || smallinput.includes('konichiba') || smallinput.includes('salute')){
   reply (`Konichiwa ${pushname}, I am ${BotName}. How can i help you?`);
 }
 
-
 if (smallinput=='a17') {
-    reply ('Yes I am Alive 🫂')
+    reply ('Yes I am Alive ')
 }
 
 if (smallinput=='sasha') {
-  reply ('Only you...🫶🏻')
+  reply ('Only you...🏻')
 }
 
 if (smallinput=='ping') {
     reply (`Hey ${pushname} Pong ${latensie.toFixed(4)} ms`)
 }
 
-
 if (smallinput.includes('good morning') || smallinput.includes('ohayo')) {
   reply (`Good morning to you too ${pushname} ☺️. Have a great day 😇.`);
 }
 
 if (smallinput.includes('good afternoon') || smallinput.includes('konnichiwa')) {
-
   reply (`Good afthernoon to you too ${pushname} ✨. Wishing you an enjoyable afternoon too 😇🤞🏻.`);
 
 }
-
-
-//if (smallinput.includes('good afthernoon')) {
- // reply (`Huh ${pushname} 😇. Wishing you an enjoyable afternoon too.`);
-//   }
-
 
 if (smallinput.includes('good night')) {
   reply (`Good night to you too ${pushname} 😇. Sleep well and sweet dreams.`);
