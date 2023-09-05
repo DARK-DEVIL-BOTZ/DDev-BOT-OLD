e6990db3a8b679818c489ac7d2f116c5981197e9
TEAM DARK DEVIL <111672563+DARK-DEVIL-BOTZ@users.noreply.github.com>
2023-09-05 17:41:46 +0530

    💝DARK-DEVIL-BOTZ🧜‍♂️

--- config.js
+++ config.js
@@ -10,16 +10,16 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = true; //auto tying in gc (true to on, false to off)
-global.autoRecord = true; //auto recording (true to on, false to off)
+global.autoTyping = false; //auto tying in gc (true to on, false to off)
+global.autoRecord = false; //auto recording (true to on, false to off)
 
 
 //
 global.Owner = ["94703892615","94767880946"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
 global.OwnerNumber = ["94703892615"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
 global.ownertag = ["94703892615"];
-global.OwnerName = "DARK DEVIL";
-global.BotName = "DDEV";
+global.OwnerName = "DARKDEVIL";
+global.BotName = "DDev";
 global.packname = "A17 Bot";  //Do not change.
 global.author = "By: Kai";   //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17";  //Do not change.
